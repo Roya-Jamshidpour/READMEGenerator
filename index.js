5a6ad0de23fcc7516e91edf1fe3b5619d3349e95
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-06-02 16:37:07 -0500

    added gitignore and some prompts

--- index.js
+++ index.js
@@ -6,13 +6,67 @@ const fs = require('fs');
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Enter a description of your project.',
+        },
+        {
+            type: 'input',
+            name: 'Installation Instructions',
+            message: 'Enter installation instructions for your project.',
+        },
+        {
+            type: 'input',
+            name: 'Usage Information',
+            message: 'Enter usage information for your project',
+        },
+        {
+            type: 'multi-choice',
+            name: 'License',
+            message: 'Which license would you like to have for your project?',
+            rows: [
+                        {
+                            name: 'Apache',
+                            value: '#f00',
+                        },
+                        {
+                            name: 'MIT',
+                            value: '#0f0',
+                        },
+                        {
+                            name: 'GPL',
+                            value: '#00f',
+                        },
+                    ],
+                },
+                {
+                    type: 'input',
+                    name: 'linkedin',
+                    message: 'Enter your LinkedIn URL.',
+                },
+                {
+                    type: 'input',
+                    name: 'github',
+                    message: 'Enter your GitHub Username',
+                },
+                {
+                    type: 'input',
+                    name: 'linkedin',
+                    message: 'Enter your LinkedIn URL.',
+                },
+            ])
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
 
 # Title
 
@@ -33,3 +87,36 @@ init();
 ## Tests  
 
 ## Questions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO: Create a function to initialize app
+        function init() { }
+
+        // Function call to initialize app
+        init();
